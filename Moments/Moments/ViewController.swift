007b6f98123d3bdde10ab8e10d6af8e63a2f1725
zhouzhenjie <zhouzhenjie@yffshun.cn>
2023-01-06 17:23:46 +0800

    添加主工程壳

---
+++ Moments/Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by Abel on 2023/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
